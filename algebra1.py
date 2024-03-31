ef8ccb193142fe50743594ecf468324e926dbd20
richard-alan-harris <164779268+richard-alan-harris@users.noreply.github.com>
2024-03-31 11:23:58 +0100

    1st commit, sim equations, no function

---
+++ algebra1.py
@@ -0,0 +1,14 @@
+#simultaneous equation
+#n1/d1 = n2/d2
+#if you have 3 you can work out the 4th
+
+n1 = 10
+d1 = 5
+n2 = 7
+d2 = 0
+
+if n2 == 0:
+    print (d2 * n1 / d1)
+elif d2 == 0:
+    print (n2 * d1 / n1)
+

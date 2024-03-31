96c1b32ecc000fe9151c3e474af6cdbc40c09bd5
richard-alan-harris <164779268+richard-alan-harris@users.noreply.github.com>
2024-03-31 20:36:35 +0100

    dec to frac works: still needs to be reduced

--- python-algebra/decimal_to_fraction.py
+++ python-algebra/decimal_to_fraction.py
@@ -1,8 +1,19 @@
-dec = input('enter a decimal number between 0 and 0.99')
-declist = []
-for i in dec:
-    declist.append (i)
+def dec_to_frac (decimal):
+    
+    declist = []
+    for i in str(decimal):
+        declist.append (i)
+    del declist[0:2]
 
-print(declist)
-del declist[0:2]
-print(declist)
+    x = len(declist)
+    if x == 1:
+        dnom = 10
+    elif x == 2:
+        dnom = 100
+    elif x == 3:
+        dnom = 1000
+    decstr = str(decimal)
+    fraction = decstr[2:],'/', dnom
+    return fraction
+
+print(dec_to_frac(0.34))

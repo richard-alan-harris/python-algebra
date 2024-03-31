841b3371f2d2040d419b2b7390394a8f1fb29a04
richard-alan-harris <164779268+richard-alan-harris@users.noreply.github.com>
2024-03-31 11:31:51 +0100

    built into function named "simequation"

--- algebra1.py
+++ algebra1.py
@@ -2,13 +2,15 @@
 #n1/d1 = n2/d2
 #if you have 3 you can work out the 4th
 
-n1 = 10
-d1 = 5
-n2 = 7
-d2 = 0
 
-if n2 == 0:
-    print (d2 * n1 / d1)
-elif d2 == 0:
-    print (n2 * d1 / n1)
 
+def simequation(n1,d1,n2,d2):
+    
+    if n2 == 0:
+        return (d2 * n1 / d1)
+    elif d2 == 0:
+        return (n2 * d1 / n1)
+    else:
+        print('please use n2 or d2 for unknown figure')
+
+print(simequation(10,5,7,0))
